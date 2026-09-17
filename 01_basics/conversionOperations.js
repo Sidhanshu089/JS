536147db34839fff653e4830252bec67ae295600
Sidhanshu089 <sidhanshunandal348@gmail.com>
2026-09-17 09:57:27 +0000

    Conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,29 @@
+// let score = "44abc"
+// console.log(typeof score)
+// console.log(typeof(score)) 
+
+// let valueNumber = Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33"--> 33
+// "33abc"--> NaN
+// null--> 0
+// undefined --> NaN
+// true --> 1
+// false --> 0
+
+// let isLoggedIn = "sid"
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+// 1--> true
+// "" --> false
+// "sid" --> true
+
+let somenumber = 56
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+

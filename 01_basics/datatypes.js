1297d1fe8134fd598cbe29a371f863f0dec0499b
Sidhanshu089 <sidhanshunandal348@gmail.com>
2026-09-17 09:43:16 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // it treats JS code as newer version
+
+// alert(3 + 3) // we are using node js not browser
+
+let name = "sid"
+let age = 21
+let isLoggedIn = false
+
+// number --> 2 to power 53
+// bigint
+// string --> " _ "
+// boolean --> true , false
+// null --> standalone
+// undefined --> value is not assigned
+// symbol --> when talking about unique
+
+// object
+
+console.table([typeof"sid", typeof undefined, typeof null])

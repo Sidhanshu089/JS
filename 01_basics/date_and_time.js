cce5724f8d06188df6cfc38c68596deb22ce2505
Sidhanshu089 <sidhanshunandal348@gmail.com>
2026-09-18 07:22:34 +0000

    Date and time

---
+++ 01_basics/date_and_time.js
@@ -0,0 +1,21 @@
+// Date
+
+let mydate = new Date()
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(typeof mydate);
+
+// let mycrereateddate = new Date(2005,3,4)
+// let mycrereateddate = new Date(2005,3,4,5,9)
+// let mycrereateddate = new Date("2005,04,04")
+let mycrereateddate = new Date("04,04,2005")
+// console.log(mycrereateddate.toLocaleString());
+
+let myTimestamp = Date.now()
+console.log(myTimestamp);
+
+
+
+
+   
